ce26c2c2be2279c58b883b51e94827a6895367ca
Navanee <navaneekrish164@gmail.com>
2022-09-09 13:45:42 +0530

    js

---
+++ datatypes/main.js
@@ -0,0 +1,51 @@
+// string
+
+let a = "navanee"
+let b = "krishnan"
+
+console.log("string");
+
+
+// undefined
+
+console.log(x);
+var x =10;
+
+// number
+ var x = 10;
+ var y = 20;
+ var z =(x+y);
+ console.log(z);
+
+
+//  boolean
+var b1 = "500";
+var a1 = "true"
+
+var nav =(b1==a1)
+var vavi=(b1===a1)
+console.log(nav);
+console.log(vavi);
+
+
+// nan
+const t = 12;;
+const s = "nn";
+const q =(t*s);
+
+console.log(q);
+
+// object
+var student ={
+    boy:1,
+    girl:2
+}
+console.log(student);
+
+// array
+var num = [1,2,3,4];
+console.log(num);
+
+// null
+var obj = null;
+console.log(obj);
